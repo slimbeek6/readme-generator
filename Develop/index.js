3b95a288725aeb13b490e0efe39bf9d6d6cc087d
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-14 14:53:36 -0800

    created first 3 questions, and did quick inquirer call to make sure of initial setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,18 +10,32 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0].q1,
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: questions[1].q2,
+                name: 'projName',
+            },
+        ])
+        .then((response) => {
+            fileName = response.projName;
+            data = response.name;
+            console.log(fileName, data);
+    });
+}
+
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: questions[0].q1,
-            name: 'name',
-        },
-    ]);
+    writeToFile();
 }
 
 // Function call to initialize app
